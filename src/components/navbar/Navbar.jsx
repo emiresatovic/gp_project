b9c7ae33c4cebb5146fed6419964a900d2d882f6
Emir Esatović <165719085+emiresatovic@users.noreply.github.com>
2024-12-16 15:37:02 +0100

    16.12 15h

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         </div>
         <div className='gpt3__navbar-links_container'>
           <p><a href="#home">Home</a></p>
-          <p><a href="#whatgpt3">What is GPT?</a></p>
+          <p><a href="#wgpt3">What is GPT?</a></p>
           <p><a href="#possibilty">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="blog">Library</a></p>
